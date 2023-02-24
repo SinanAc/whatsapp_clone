bc17e064cbfbfdedf2b8f80bb0a8d6c4541aed85
Sinan <sinanac124@gmail.com>
2023-02-24 22:51:31 +0530

    initial_ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/colors.dart';
+import 'package:whatsapp_clone/screens/mobile_layout_screen.dart';
+import 'package:whatsapp_clone/screens/web_layout_screen.dart';
+import 'package:whatsapp_clone/utils/responsive_layout.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Whatsapp UI',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+      ),
+      home: const ResponsiveLayout(
+        mobileScreenLayout: MobileLayoutScreen(),
+        webScreenLayout: WebLayoutScreen(),
+      ),
+    );
   }
 }
